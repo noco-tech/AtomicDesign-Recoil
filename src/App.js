809a802e0a3a0cc0a80d0656126a50d5adfa04ba
sadainoaraisan <73414778+sadainoaraisan@users.noreply.github.com>
2022-05-07 21:09:50 +0900

    organismsの作成 (UserCard) (atomにはCard}

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
+
+const user = {
+  name: "おれ",
+  image: "https://source.unsplash.com/_PzCi_UwKso",
+  email: "12345@example.com",
+  phone: "000-0000-0000",
+  company: {
+    name: "test株式会社"
+  },
+  website: "https://google.com"
+};
 
 export default function App() {
   return (
@@ -10,6 +22,7 @@ export default function App() {
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
